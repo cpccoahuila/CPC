6051f1411616c6a40cae58b95c5f4f148f1ee517
Renelzm <Renelzm@users.noreply.github.com>
2023-03-16 22:46:08 +0000

    deploy: ed7c3d8ca304017e22dcacc1bd2662f46eead7fa

---
+++ _nuxt/static/1679006751/contacto/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCPC\u002F_nuxt\u002Fstatic\u002F1679006751",layout:"default",error:a,state:{storage:{}},serverRendered:true,routePath:"\u002Fcontacto",config:{_app:{basePath:"\u002FCPC\u002F",assetsPath:"\u002FCPC\u002F_nuxt\u002F",cdnURL:a}}}}(null));
