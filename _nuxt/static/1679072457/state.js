509d87262983907ca059e5539b8f71f19e48a52e
ednamtz <ednamtz@users.noreply.github.com>
2023-03-17 18:00:49 +0000

    deploy: 3f35bda79bd5e85352f592111978f7f9ffe2a806

--- _nuxt/static/1679072457/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCPC\u002F_nuxt\u002Fstatic\u002F1679072457",layout:"default",error:a,state:{storage:{}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FCPC\u002F",assetsPath:"\u002FCPC\u002F_nuxt\u002F",cdnURL:a}}}}(null));
