c81dcd95578f8f48bd3eb97fcbea9beee13852ce
ednamtz <ednamtz@users.noreply.github.com>
2023-03-17 17:01:15 +0000

    deploy: 557883c94614e937d9b0942ae4902f09aeffc708

--- _nuxt/static/1679070029/convocatorias/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCPC\u002F_nuxt\u002Fstatic\u002F1679070029",layout:"default",error:a,state:{storage:{}},serverRendered:true,routePath:"\u002Fconvocatorias",config:{_app:{basePath:"\u002FCPC\u002F",assetsPath:"\u002FCPC\u002F_nuxt\u002F",cdnURL:a}}}}(null));
