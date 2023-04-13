a7bfaa06f2ac6c5021c051bbe248faa0a183b0c6
Renelzm <Renelzm@users.noreply.github.com>
2023-04-13 22:35:25 +0000

    deploy: beae379ac52324638b379f66ad396d29bbc03f14

--- _nuxt/static/1681423452/indicadores/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681423452",layout:"default",error:a,state:{storage:{}},serverRendered:true,routePath:"\u002Findicadores",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
