7d0bd2d6677870044e9ce8a63a96c078b96b2723
Renelzm <Renelzm@users.noreply.github.com>
2023-04-13 21:46:35 +0000

    deploy: 22d06e1c5aa4c8b1ec96c4dd625a4d0923f802e2

---
+++ _nuxt/static/1681422378/redCiudadana/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681422378",layout:"default",error:a,state:{storage:{}},serverRendered:true,routePath:"\u002FredCiudadana",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
