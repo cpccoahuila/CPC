e4561436a2e3a6d7a1f03ae9838b01fcedc62fdc
ednamtz <ednamtz@users.noreply.github.com>
2023-09-12 18:31:47 +0000

    deploy: 9078ad7c1b51f1628c80b02fd2b9d2acad693d70

--- _nuxt/static/1694542221/redCiudadana/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1694542221",layout:"default",error:a,state:{storage:{}},serverRendered:true,routePath:"\u002FredCiudadana",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
