6197e88041b28409cc3af6748a7551dfba2835ff
ednamtz <edna.martinez@outlook.es>
2024-02-06 19:18:41 -0600

    Se agregan lineamientos

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false }
+    { src: '~/plugins/vuex-persist', ssr: false },
+    { src: '~/plugins/vue-gtag.js', ssr: false}
     
   ],
 
---
+++ plugins/vue-gtag.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'G-X0XQ5LDP8K' }
+})
