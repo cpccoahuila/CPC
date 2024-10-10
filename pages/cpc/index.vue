b68fcea932ae6e7b9c3289ede1aefa1d0d9ed477
ednamtz <edna.martinez@outlook.es>
2024-10-09 22:57:55 -0600

    Se agrega Yolanda como nueva presidenta y se elimina a Karla

--- pages/cpc/index.vue
+++ pages/cpc/index.vue
@@ -256,16 +256,10 @@ export default {
                 //img: 'cc-cpc.png',
                 //mail: 'jafia.pacheco@cpccoahuila.org.mx'
            // },
-            {
-                nombre: 'Miguel Francisco Crespo Alvarado',
-                cargo: 'Consejero Presidente del Consejo de Participación Ciudadana.',
-                foto: 'cpc-mfca.jpg',
-                img: 'cc-cpc.png',
-                mail: 'miguel.crespo@cpccoahuila.org.mx'
-            },
+            
             {
                 nombre: 'Yolanda Guadalupe Montes Martínez',
-                cargo: 'Consejera del Consejo de Participación Ciudadana.',
+                cargo: 'Consejera Presidente del Consejo de Participación Ciudadana.',
                 foto: 'cpc-ygmm.jpg',
                 img: 'cc-cpc.png',
                 mail: 'yolanda.montes@cpccoahuila.org.mx'
@@ -277,13 +271,7 @@ export default {
                 img: 'cc-cpc.png',
                 mail: 'carlos.guzman@cpccoahuila.org.mx'
             },
-            {
-                nombre: 'Karla Samperio Flores',
-                cargo: 'Consejera  del Consejo de Participación Ciudadana.',
-                foto: 'cpc-ksf.png',
-                img: 'cc-cpc.png',
-                mail: 'karla.samperio@cpccoahuila.org.mx'
-            },
+          
 
             ],
             Valores: [
