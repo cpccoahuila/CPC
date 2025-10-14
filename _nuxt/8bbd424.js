08e5b00aea9ac9a545d0dcc009d927777119ad2e
ednamtz <ednamtz@users.noreply.github.com>
2025-10-14 18:13:56 +0000

    deploy: 410e50222017c2d4c35838d3f4c43ecc2275fc1b

---
+++ _nuxt/8bbd424.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{712:function(e,n,r){"use strict";r.r(n);var o=r(615);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.17.1","app-compat")}}]);
